791ad0ecd0482d9e57c0be7398154bb7b0b5e644
Ashwitha <jathanashwitha@gmail.com>
2023-02-14 10:56:19 +0530

    message

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import images from "./assets/images.webp";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  const x=9;
+  var text="hello";
+  if(x==10){
+    text="Equal";
+  }
+  else{
+    text="not equal";
+  }
+  return <div className='App'>
+    <h1>Hello world</h1>
+    <p>this is paragraph</p>
+    <p>{5+55}</p>
+    <img src={images} alt="image"/>
+    <h2>{text}</h2>
+
+
+
+    <h2>{x>10? "greater":"lesser"}</h2>
     </div>
-  );
+  
 }
 
 export default App;
